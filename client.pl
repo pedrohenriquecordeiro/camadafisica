fb3c7f1b443e07a20ee389b555eb0740c0744ef3
PedroCordeiro <pedro.henrique.cefetmg@gmail.com>
2018-09-10 20:16:24 -0300

    pdu em binario

--- client.pl
+++ client.pl
@@ -16,11 +16,12 @@ open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Nao foi possivel abrir o arquivo '$filename' $!";
  
 #salva o conteudo da primeira linha em uma variavel
-my $data = <$fh>;
+my $data_file = <$fh>;
+#converte o conteudo para binario
+my $data_file_bin = sprintf unpack("b*",$data_file );
 
-
-my $serveraddr='127.0.0.1';
-my $clientaddr=eval{Net::Address::IP::Local->public_ipv4};
+my $serveraddr = '127.0.0.1';
+my $clientaddr = eval{Net::Address::IP::Local->public_ipv4};
 
 #temos que filtrar o endereco ip que esta no arquivo
 #cria o socket
@@ -32,17 +33,41 @@ $socket = new IO::Socket::INET (
 
 #print "Conectado com o servidor.\n";
 
-
+# preambulo da pdu = 7 bytes
 my $preambulo = '10101010101010101010101010101010101010101010101010101010';
-my $start_frame = '10101011';
-my ($smac) = `arp -a $serveraddr` =~ /at\s+(\S+)\s+/;
-my ($cmac) = `arp -a $clientaddr` =~ /at\s+(\S+)\s+/;
-my $tipo = '0000000011111111';
+my $preambulo_bin = sprintf unpack("b*",$preambulo);
 
-$data= $preambulo.$start_frame.$smac.$cmac.$tipo.$data;
+# início do delimitador de Quadro = 1 byte
+my $start_frame = '10101011';
+my $start_frame_bin = sprintf unpack("b*",$start_frame);
+
+# endereco mac do servidor = > 6 bytes
+my $mac = `arp -a $serveraddr`;
+if($mac =~ m/(\w\w-\w\w-\w\w-\w\w-\w\w-\w\w) | (\w\w:\w\w:\w\w:\w\w:\w\w:\w\w) /){
+	$mac = $1;
+}
+my $mac_bin = sprintf unpack("b*",$mac);
+
+# endereco mac do destino => 6 bytes
+my $cmac = `arp -a $clientaddr` =~ /at\s+(\S+)\s+/;
+if($cmac =~ m/(\w\w-\w\w-\w\w-\w\w-\w\w-\w\w) | (\w\w:\w\w:\w\w:\w\w:\w\w:\w\w) /){
+	$cmac = $1;
+}
+my $cmac_bin = sprintf unpack("b*",$cmac );
+
+# dado a ser enviado 1000 bytes
+
+# tamanho de todo o quadro => 2 bytes
+# total => 1022 bytes -> 8176 bits
+my $length = '0001111111110000';
+my $length_bin = sprintf unpack("b*",$length );
+
+# concatena o os campos da pdu
+$data_bin = $preambulo_bin.$start_frame_bin.$smac_bin.$cmac_bin.$length_bin.$data_file_bin;
+
 
 #envia o quadro
-my $thread_1 = threads->create(\&enviando_mensagem,$socket) 
+my $thread_1 = threads->create(\&enviando_mensagem,$socket,$data_in_bin) 
 	or die "Erro na criacao da thread de espera";
 
 $thread_1 ->join() or die "Fechado com sucesso!\n";
@@ -58,6 +83,7 @@ sub enviando_mensagem{
 	# pegar parametros passados
 	my @s = @_ ;
 	my $socket = $s[0];
+	my $data_bin = $s[1];
 	
 	#
 	# nesse ponto simulamos uma colisao
@@ -88,7 +114,7 @@ sub enviando_mensagem{
 		}
 		
 		#envia os dados
-		print $socket "$data\n";
+		print $socket "$data_bin\n";
 		
 		#resposta do servidor
 		my $mensagem_do_servidor = <$socket>;
